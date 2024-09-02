b08bfb4c02df39db81eb8afa843fd62bbcf81f72
naveenkanuganti <naveenk.kanuganti@gmail.com>
2024-09-01 20:52:43 -0500

    Add repo_info for deployment file

--- naveen_wp/create_deployment.py
+++ naveen_wp/create_deployment.py
@@ -3,7 +3,7 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/NAVEENKANUGANTI/prefect_pratice_new.git",
-        entrypoint="my_gh_wf.py:prefect_pratice_new",
+        entrypoint="my_gh_wf.py:repo_info",
     ).deploy(
         name="my-first-deployment",
         work_pool_name="my-work-pool",
